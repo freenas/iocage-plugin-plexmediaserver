498d91141e6166f77c6c269a410fd3ab270a42e1
Martin Wilke <miwi@ixsystems.com>
2018-11-06 02:46:23 +0800

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -3,5 +3,8 @@
 # Enable the service
 sysrc -f /etc/rc.conf plexmediaserver_enable="YES"
 
+#Try to make filesystem readable
+sysrc -f /etc/rc.conf plexmediaserver_plexpass_support_path="/"
+
 # Start the service
 service plexmediaserver restart 2>/dev/null
