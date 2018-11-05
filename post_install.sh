fdb6a2caba493b228ec536d9d6387584f70f8da4
Martin Wilke <miwi@ixsystems.com>
2018-11-06 02:48:55 +0800

    Fix  plexmediaserver_support_path

--- post_install.sh
+++ post_install.sh
@@ -4,7 +4,7 @@
 sysrc -f /etc/rc.conf plexmediaserver_enable="YES"
 
 #Try to make filesystem readable
-sysrc -f /etc/rc.conf plexmediaserver_plexpass_support_path="/"
+sysrc -f /etc/rc.conf plexmediaserver_support_path="/"
 
 # Start the service
 service plexmediaserver restart 2>/dev/null
